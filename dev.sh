1342bba3883acc8cdd8208b14e0e35037e639b10
Eddy Varela <eddvar@amazon.com>
2023-05-03 10:51:19 -0400

    Delete dev.sh

--- dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eo pipefail
-source .circleci/local_publish_helpers.sh
-startLocalRegistry "$(pwd)/.circleci/verdaccio.yaml"
-setNpmRegistryUrlToLocal
-changeNpmGlobalPath
-generatePkgCli linux
-unsetNpmRegistryUrl
